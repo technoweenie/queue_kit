0f13efb9ed78ad66ff5535f721be79ff688f2059
risk danger olson <technoweenie@gmail.com>
2013-04-17 16:15:24 -0600

    a simple example to show how the signal checker works

---
+++ examples/rand_worker.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../../lib/queue_kit", __FILE__)
+require File.expand_path("../../lib/queue_kit/signal_handlers/graceful_quit", __FILE__)
+
+queue = Object.new
+def queue.pop
+  rand 10
+end
+
+worker = QueueKit::Worker.new :queue => queue
+worker.on_pop do |num|
+  puts num
+end
+
+worker.after_work { sleep 1 }
+
+QueueKit::SignalChecker.trap(worker, QueueKit::GracefulQuit)
+
+puts "Starting on #{Process.pid}..."
+worker.run
+
